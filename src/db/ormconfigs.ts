282cba72114deeb2aaab1d2f1766b9a21c38c228
IqueMoraes <iquedemoraes@hotmail.com>
2022-05-25 15:58:11 -0300

    create basics configs, dirs and database connection

---
+++ src/db/ormconfigs.ts
@@ -0,0 +1,38 @@
+import { ConnectionOptions } from 'typeorm';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const prodConfig = {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  logging: false,
+  entities: [path.join(__dirname, '../entities/**/*.*')],
+  migrations: [path.join(__dirname, '../migrations/**/*.*')],
+  cli: {
+    entitiesDir: path.join(__dirname, '../entities'),
+    migrationsDir: path.join(__dirname, '../migrations'),
+  },
+  ssl: { rejectUnauthorized: false },
+} as ConnectionOptions;
+
+const devConfig = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  logging: false,
+  entities: [path.join(__dirname, '../entities/**/*.ts')],
+  migrations: [path.join(__dirname, '../migrations/**/*.ts')],
+  cli: {
+    entitiesDir: path.join(__dirname, '../entities'),
+    migrationsDir: path.join(__dirname, '../migrations'),
+  },
+  ssl: false,
+} as ConnectionOptions;
+
+// eslint-disable-next-line no-nested-ternary
+export default process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata';
+import dotenv from 'dotenv';
+import { createConnection } from 'typeorm';
+import app from './app';
+
+import dbConfig from './db/ormconfigs';
+
+dotenv.config();
+
+createConnection(dbConfig)
+  .then(() => {
+    const PORT = process.env.PORT ?? 3000;
+    console.log('Database connected');
+
+    app.listen(PORT, () => {
+      console.log(`App is running on port http:localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
