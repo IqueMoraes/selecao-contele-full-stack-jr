6a51841bdb9a2b3a601aad91e92454d04657753f
IqueMoraes <iquedemoraes@hotmail.com>
2022-05-25 17:10:44 -0300

    remove column 'name' from user entity

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -9,9 +9,6 @@ export class Users {
   @PrimaryGeneratedColumn('uuid')
     id:string;
 
-  @Column({ length: 128 })
-    name: string;
-
   @Column({ length: 128, unique: true })
     email: string;
 
